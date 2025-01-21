774e9fd5b9ea77f279f846e9ace18d9d10ac70ed
anonymous298 <tackletalha@gmail.com>
2025-01-21 11:12:30 +0500

    tutorial1

--- tutorial1.py
+++ tutorial1.py
@@ -1,26 +1,10 @@
 import cv2
 
-image = cv2.imread('assets/humans.jpg')
-# print(image.shape)
+img = cv2.imread('assets/picofme.png')
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img = cv2.resize(img, (400, 400))
+# img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
 
-
-resized = cv2.resize(image, (400, 400))
-
-# gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
-# print(cv2.COLOR_BGR2GRAY)
-
-# flipped = cv2.flip(gray, -1)
-
-# edges = cv2.Canny(resized, 100, 200)
-
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-
-gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
-
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-
-for (x, y, w, h) in faces:
-    cv2.rectangle(resized, (x, y), (x+w, y+h), (255, 0, 0), 2)
-
-cv2.imshow('image', resized)
-cv2.waitKey()
+cv2.imshow('image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
